565a159e9512d00878739420a7afe780ffa09020
Eric Ong <ako.si.eric@gmail.com>
2021-04-25 15:53:40 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -16,8 +16,17 @@ input.onButtonPressed(Button.B, function () {
     basic.pause(1000)
     comment.kesu(comment.init(DigitalPin.P0, DigitalPin.P14))
 })
+pins.servoWritePin(AnalogPin.P2, 180)
+pins.servoWritePin(AnalogPin.P2, 0)
 basic.showString("Signal")
 basic.clearScreen()
 basic.forever(function () {
-	
+    if (pins.digitalReadPin(DigitalPin.P1) == 1) {
+        pins.servoWritePin(AnalogPin.P2, 180)
+        basic.pause(500)
+        basic.showIcon(IconNames.Surprised)
+        pins.servoWritePin(AnalogPin.P2, 0)
+        basic.pause(500)
+        basic.clearScreen()
+    }
 })
